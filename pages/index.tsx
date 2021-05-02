d9502a02a72cb50ca3f35e9314d616cfba6297f8
coffeetea99 <coffeetea99iro@gmail.com>
2021-05-02 14:03:26 +0900

    create title page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 function Home() {
   return (
-    <>Hello, World!</>
+    <>
+      <h3>The Humanist Software Engineer</h3>
+      <h1>Jinje Han</h1>
+      <a href='/diary'>diary</a>
+      <br />
+      <a href='/anime'>anime</a>
+      <br />
+    </>
   );
 }
 
-export default Home
+export default Home;
