b64470609b5c18ac2904236c2c3fc571231c9d0d
coffeetea99 <coffeetea99iro@gmail.com>
2021-05-09 19:00:52 +0900

    change favicon & title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Coffeetea / Jinje Han</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
